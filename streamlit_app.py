53751a13ea37337cf8372e0e7f100d441582bff3
lindseyspoden <151679304+lindseyspoden@users.noreply.github.com>
2023-12-06 11:39:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
